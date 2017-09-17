3e25ceee7043bb65b65634afb24d17c8e36183c4
Bradley Franz <bfranz@innovia.com>
2017-09-17 07:21:22 -0500

    Added Simple PowerShell script

---
+++ HelloWorld/HelloWorld.ps1
@@ -0,0 +1,10 @@
+CD -Path $Env:USERPROFILE\Desktop\Work\
+
+MOVE Objects\CODEUNIT\Codeunit*.txt Objects
+MOVE Objects\MENUSUITE\Menusuite*.txt Objects
+MOVE Objects\PAGE\Page*.txt Objects
+MOVE Objects\REPORT\Report*.txt Objects
+MOVE Objects\TABLE\Table*.txt Objects
+MOVE Objects\XMLPORT\Xmlport*.txt Objects
+MOVE Objects\QUERY\Query*.txt Objects
+MOVE Objects\LAYOUT\Layout*.txt Objects
